fd4b9e5de9b5a38c22e67c6e20852094f52cf384
Johnfietsen <lucstefelmanns@live.nl>
2018-01-15 14:50:52 +0100

    JSON file voor netwerk klaar.

---
+++ process_report.md
@@ -0,0 +1,14 @@
+## Progress report
+
+###### Study:       Minor Programming, University of Amsterdam
+###### Course:      Programmeerproject
+###### Name:        Luc Stefelmanns
+###### Student nr.: 10669124
+
+#### Week 1
+##### Monday
+##### Tuesday
+##### Wednesday
+##### Thursday
+##### Friday
+##### Weekend
--- scripts/CSVtoJSON.py
+++ scripts/CSVtoJSON.py
@@ -5,6 +5,13 @@
 # Student nr.:	10669124
 
 
+import csv
+import json
+
+
+MAX_LINKS = 10
+
+
 def CSVtoJSON(algorithm):
 
 	freespaces = 	open('C:/Users/lucst/Desktop/Minor programmeren/GitHub/' + \
@@ -37,13 +44,13 @@ def CSVtoJSON(algorithm):
 	tmp_type =			[]
 	tmp_value =			[]
 
-	network = {'nodes' : [], 'links' : []}
+	network =			[]
 
 	for line in freespaces:
 		tmp_freespaces.append(line.split(','))
 	for i in range(len(tmp_freespaces)):
-		print(tmp_freespaces[i])
-		tmp_freespaces[i] = tmp_freespaces[i].split(';')
+		for j in range(len(tmp_freespaces[i])):
+			tmp_freespaces[i][j] = tmp_freespaces[i][j].split(';')
 
 	for line in location:
 		tmp_location.append(line.split(','))
@@ -51,7 +58,8 @@ def CSVtoJSON(algorithm):
 	for line in neighbours:
 		tmp_neighbours.append(line.split(','))
 	for i in range(len(tmp_neighbours)):
-		tmp_neighbours[i] = tmp_neighbours[i].split(';')
+		for j in range(len(tmp_neighbours[i])):
+			tmp_neighbours[i][j] = tmp_neighbours[i][j].split(';')
 
 	for line in self_id:
 		tmp_id.append(line.split(','))
@@ -59,13 +67,60 @@ def CSVtoJSON(algorithm):
 	for line in size:
 		tmp_size.append(line.split(','))
 	for i in range(len(tmp_size)):
-		tmp_size[i] = tmp_size[i].split(';')
+		for j in range(len(tmp_size[i])):
+			tmp_size[i][j] = tmp_size[i][j].split(';')
 
 	for line in self_type:
 		tmp_type.append(line.split(','))
 
 	for line in value:
 		tmp_value.append(line.split(','))
+
+
+	for i in range(len(tmp_id)):
+
+		network.append({'nodes' :	[],
+						'links' :	[]})
+
+		network[i]['nodes'].append({'id' : 1001, 'type' : 'side'})
+		network[i]['nodes'].append({'id' : 1002, 'type' : 'side'})
+		network[i]['nodes'].append({'id' : 1003, 'type' : 'side'})
+		network[i]['nodes'].append({'id' : 1004, 'type' : 'side'})
+
+		for j in range(len(tmp_id[i])):
+			if tmp_id[i][j] != '\n':
+				network[i]['nodes'].append({'id' :		tmp_id[i][j],
+											'type' :	tmp_type[i][j]})
+
+
+	for i in range(len(tmp_id)):
+
+		for l in range(MAX_LINKS):
+
+			network[i]['links'].append([])
+
+			for j in range(len(tmp_id[i])):
+
+				for k in range(l):
+
+					if tmp_id[i][j] != '\n':
+						network[i]['links'][l].append({
+									'source' :	int(tmp_id[i][j]),
+									'target' :	int(tmp_neighbours[i][j][k]),
+									'dist' :	float(tmp_freespaces[i][j][k])
+									})
+
+	# for line in network:
+	# 	for link in line['links']:
+	# 		print(link)
+
+	JSON_algorithm = {'network' : network}
+
+	JSON_data = json.loads(json.dumps(JSON_algorithm))
+
+	with open('C:/Users/lucst/Desktop/Minor programmeren/GitHub/' + \
+			  'Programmeerproject/JSON/' + algorithm + '.json', 'w') as outfile:
+	    json.dump(JSON_data, outfile)
 
 
 if __name__ == '__main__':
