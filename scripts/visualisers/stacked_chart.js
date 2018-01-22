53c74064cada88258bb593f8c13c55228a89b1ec
Johnfietsen <lucstefelmanns@live.nl>
2018-01-23 00:55:52 +0100

    Frame.

--- scripts/visualisers/stacked_chart.js
+++ scripts/visualisers/stacked_chart.js
@@ -9,6 +9,16 @@ var area = d3.area()
 			    .y0(function(d) { return y(d.min_temp); })
 			    .y1(function(d) { return y(d.max_temp); });
 
+
+// define the line
+var line = d3.line()
+			    .x(function(d) { return x(d.i); })
+			    .y(function(d) { return y(d.value); });
+
+// set the ranges
+var x = d3.scaleTime().range([0, widthStacked]);
+var y = d3.scaleLinear().range([heightStacked, 0]);
+
 function createStackedChart(algorithm) {
 
 	d3.json("/data/json/" + algorithm + ".json", function(error, hillclimber) {
@@ -16,9 +26,19 @@ function createStackedChart(algorithm) {
 
 		var stackedSVG = d3.select("body").select("#stacked");
 
-		var area = stackedSVG.append("g")
-					.attr("class", "area")
-					.selectAll("area")
-					.data(hillclimber.stacked)
-					.enter().append("area");
+		stackedSVG
+			.selectAll(".path")
+			.data(hillclimber.stacked)
+			.enter().append("path")
+			.attr("class", "line")
+			.style("stroke", function(d) { return d.color = colour(d.type); })
+			.attr("d", function(d) { return line(d); });
+
+		// var area = stackedSVG.append("g")
+		// 			.attr("class", "area")
+		// 			.selectAll("area")
+		// 			.data(hillclimber.stacked)
+		// 			.enter().append("area")
+		// 			.style("fill", function(d) { console.log(d)});
+	});
 }
